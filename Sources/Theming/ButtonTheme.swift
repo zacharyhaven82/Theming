ee9e7cbfb4d029775b7148c2535d8c5568810c8d
Zachary Haven <zacharyhaven@me.com>
2025-07-15 09:52:03 -0500

    Utilize protocol

---
+++ Sources/Theming/ButtonTheme.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  Theming
+//
+//  Created by Zachary Haven on 7/14/25.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+@available(iOS 16.0, *)
+protocol ButtonThemeable {
+    var backgroundColor: Color { get set }
+    var foregroundColor: Color { get set }
+    var cornerRadius: CGFloat { get set }
+}
+
+@available(iOS 16.0, *)
+public struct DefaultButtonTheme: ButtonThemeable {
+    public var backgroundColor: Color = Color(StyleDictionaryEnum.buttonPrimaryBackground)
+    public var foregroundColor: Color = Color(StyleDictionaryEnum.buttonPrimaryText)
+    public var cornerRadius: CGFloat = CGFloat(StyleDictionaryEnum.buttonBorderRadius)
+}
